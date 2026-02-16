528562e94471c45f920db741a8277e0c309bd001
ly6712 <ly6712@srmist.edu.in>
2026-02-16 08:35:27 +0530

    Implement UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPS Banner Application - Use Case 1
+ * 
+ * @author Yogen
+ * @version 1.0
+ * @since 16-02-2026
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
